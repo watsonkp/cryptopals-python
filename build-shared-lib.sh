13be8dcf0e05db7f30c5939bc066a170704d68a7
sulliedeclat <sulliedeclat@gmail.com>
2022-08-16 15:36:18 -0400

    Updated shared library build script

--- build-shared-lib.sh
+++ build-shared-lib.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
 gcc -shared -fPIC\
-	-I/home/sulliedeclat/source/openssl-1.1.1q/build/include\
-	-L/home/sulliedeclat/source/openssl-1.1.1q/build/lib\
 	cryptowrapper.c -lcrypto\
 	-o libcryptowrapper.so
-
-# Old paths
-#gcc -shared -fPIC\
-#	-I/home/sulliedeclat/bounty/tools/openssl/build/1.1/opt/openssl/include\
-#	-L/home/sulliedeclat/bounty/tools/openssl/build/1.1/opt/openssl/lib\
-#	cryptowrapper.c -lcrypto\
-#	-o libcryptowrapper.so
