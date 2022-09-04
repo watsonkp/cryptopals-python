c3a63c9ece294aa104f6b9a867bbb8f1fdbe8c2c
sulliedeclat <sulliedeclat@gmail.com>
2022-09-04 16:29:02 -0400

    Added solution to challenge 40
    Added solution to challenge 40. Implement an E=3 RSA Broadcast attack.

--- tests/dhfriends_test.py
+++ tests/dhfriends_test.py
@@ -5,6 +5,8 @@ from sullied_cryptography_testing import srp
 from sullied_cryptography_testing import diffiehellman as dh
 from sullied_cryptography_testing import rsa
 from sullied_cryptography_testing import util
+from sullied_cryptography_testing import numbertheory
+from sullied_cryptography_testing import gmpwrapper
 
 class TestDiffieHellmanAndFriends(unittest.TestCase):
 	def test_challenge33(self):
@@ -113,3 +115,28 @@ class TestDiffieHellmanAndFriends(unittest.TestCase):
 		ciphertext = peer.encrypt(message)
 		plaintext = util.int_to_bytes(peer.decrypt(ciphertext))
 		self.assertEqual(message, plaintext)
+
+	def test_challenge40(self):
+		"""
+		Implement an E=3 RSA Broadcast attack.
+		The attacker observes the same message encrypted 3 times with 3 distinct public keys and is able to decrypt the message without knowledge of the private key. The length of the message that can be decrypted is limited by E. Larger values for E lower this limit so that it is not useful.
+		"""
+		peer = rsa.Peer()
+
+		message = "The quick brown fox jumped over the lazy dog."
+		e = 3
+
+		# Encrypt the same message with 3 different public keys.
+		public_key_1 = rsa.PublicKey(e)
+		cipher_text_1 = peer.encrypt(message, public_key=public_key_1)
+		public_key_2 = rsa.PublicKey(e)
+		cipher_text_2 = peer.encrypt(message, public_key=public_key_2)
+		public_key_3 = rsa.PublicKey(e)
+		cipher_text_3 = peer.encrypt(message, public_key=public_key_3)
+
+		# Retrieve the message from the cube root of the result of the Chinese Remainder Theorem.
+		residues = [cipher_text_1, cipher_text_2, cipher_text_3]
+		modulus = [public_key_1.n, public_key_2.n, public_key_3.n]
+		sum = numbertheory.chinese_remainder_theorem(residues, modulus)
+		plain_text = util.int_to_bytes(gmpwrapper.integer_cube_root(sum))
+		self.assertEqual(plain_text.decode('utf-8'), message)
