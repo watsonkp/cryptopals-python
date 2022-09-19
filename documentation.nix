1c295a7a4c169cb14a7dddc98b86930a021e747d
sulliedeclat <sulliedeclat@gmail.com>
2022-09-19 15:09:12 -0400

    Added initial documentation generation
    Added initial attempt at generating HTML documentation from docstrings in source
    code using Sphinx.

---
+++ documentation.nix
@@ -0,0 +1,60 @@
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5c211b47aeadcc178c5320afd4e74c7eed5c389f.tar.gz") {};
+
+# Run with
+# nix-shell documentation.nix
+# cd docs
+# make html
+
+let
+	cryptowrapper = stdenv.mkDerivation {
+		pname = "libcryptowrapper";
+		version = "0.1.0";
+		propagatedBuildInputs = [ openssl ];
+		src = ./cryptowrapper;
+
+		meta = with lib; {
+			homepage = "https://github.com/watsonkp/cryptopals-python";
+			description = "Library simplifying the use of a few OpenSSL functions.";
+		};
+	};
+	gmpwrapper = stdenv.mkDerivation {
+		pname = "libgmpwrapper";
+		version = "0.1.0";
+		propagatedBuildInputs = [ gmp ];
+		src = ./gmpwrapper;
+
+		meta = with lib; {
+			homepage = "https://github.com/watsonkp/cryptopals-python";
+			description = "Library simplifying the use of a few GNU Multiple Precision Arithmetic Library functions.";
+		};
+	};
+	cryptopals = python310.pkgs.buildPythonPackage rec {
+		pname = "cryptopals";
+		version = "0.37.0";
+		format = "pyproject";
+		src = ./.;
+
+		propagatedBuildInputs = [ cryptowrapper gmpwrapper ];
+		# WARNING: This feels absolutely disgusting.
+		# ctypes runtime loading of libraries doesn't play well with nix.
+		# https://github.com/nixos/nixpkgs/issues/7307
+		# https://discourse.nixos.org/t/screenshot-with-mss-in-python-says-no-x11-library/14534/4
+		prePatch = ''
+			sed -i 's|libcryptowrapper-0.1.0.so|${lib.makeLibraryPath [ cryptowrapper ]}/libcryptowrapper-0.1.0.so|' src/sullied_cryptography_testing/cryptowrapper.py
+			sed -i 's|libgmpwrapper-0.1.0.so|${lib.makeLibraryPath [ gmpwrapper ]}/libgmpwrapper-0.1.0.so|' src/sullied_cryptography_testing/gmpwrapper.py
+		'';
+
+		doCheck = true;
+		checkInputs = [ cryptowrapper gmpwrapper python310.pkgs.pytest ];
+		checkPhase = "pytest";
+		meta = {
+			homepage = "https://github.com/watsonkp/cryptopals-python";
+			description = "Tools to test Cryptography inspired by CTF challenges";
+		};
+	};
+	pythonEnv = python310.withPackages (ps: [ cryptopals python310.pkgs.sphinx python310.pkgs.sphinx_rtd_theme ]);
+in mkShell {
+	packages = [
+		pythonEnv
+	];
+}
